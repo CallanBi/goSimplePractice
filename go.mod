c34db3bd3a4bdb6fb10e2dd0e6325655b90fa345
CallanBi <mcbihv@126.com>
2022-04-28 17:48:43 +0800

    chore: add vscode runtime support

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module goSimplePractice
 
-go 1.16
+go 1.18
--- runelength/runelength.go
+++ runelength/runelength.go
@@ -1,19 +1,19 @@
 package runelength
 
 import (
-  "fmt"
-  "runtime"
-  "unicode/utf8"
+	"fmt"
+	"runtime"
+	"unicode/utf8"
 )
 
-func CalRuneLength()  {
-  fmt.Print("\ncalRuneLength 练习:\n\n")
-  a := `asSASA ddd dsjkdsjs dk`
-  b := `asSASA ddd dsjkdsjsこん dk`
-  fmt.Println("a 字节数", len(a)) // 统计字节，带空格
-  fmt.Println("b 字节数",len(b)) // 统计字节，带空格
-  fmt.Println("a 字符数", utf8.RuneCountInString(a))
-  fmt.Println("b 字符数", utf8.RuneCountInString(b))
+func CalRuneLength() {
+	fmt.Print("\ncalRuneLength 练习:\n\n")
+	a := `asSASA ddd dsjkdsjs dk`
+	b := `asSASA ddd dsjkdsjsこん dk`
+	fmt.Println("a 字节数", len(a)) // 统计字节，带空格
+	fmt.Println("b 字节数", len(b)) // 统计字节，带空格
+	fmt.Println("a 字符数", utf8.RuneCountInString(a))
+	fmt.Println("b 字符数", utf8.RuneCountInString(b))
 
-  fmt.Println("运行时", runtime.GOOS)
+	fmt.Println("运行时", runtime.GOOS)
 }
